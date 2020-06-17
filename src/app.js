9c55a17116e05437f87c8867fed54f934988751a
neliosilva96 <neliosilva96@gmail.com>
2020-06-17 14:25:38 +0100

    finish

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,51 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);    
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const project = {
+      id : uuid(), 
+      title, 
+      url,
+      techs,
+      likes: 0
+  };
+  repositories.push(project);
+  return response.json(project);  
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const projectindex = repositories.findIndex(project => project.id == id);
+  if(projectindex < 0)
+      return response.status(400).json({erro : "Project not found" });
+
+  repositories[projectindex]["title"] = title;
+  repositories[projectindex]["url"] = url;
+  repositories[projectindex]["techs"] = techs;
+  return response.json(repositories[projectindex]); 
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const projectindex = repositories.findIndex(project => project.id == id);
+  if(projectindex < 0)
+    return response.status(400).json({erro : "Project not found" });
+  repositories.splice(projectindex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const projectindex = repositories.findIndex(project => project.id == id);
+  if(projectindex < 0)
+    return response.status(400).json({erro : "Project not found" });
+  repositories[projectindex]["likes"] +=1;
+  response.json(repositories[projectindex]);
 });
 
 module.exports = app;
